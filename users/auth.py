597e37f8f97054864d0fff38b2c30b39a5937ace
balddrAdmin <boliklevik@gmail.com>
2026-06-13 01:45:01 +0200

    password hashing added

--- users/auth.py
+++ users/auth.py
@@ -1,3 +1,13 @@
 from settings import get_token_data
+from passlib.context import CryptContext
 
 TOKEN_DATA = get_token_data()
+crypt_context = CryptContext('bcrypt')
+
+
+def get_hashed_password(password: str):
+    return crypt_context.hash(password)
+
+
+def verify_password(password: str, hashed_password: str):
+    crypt_context.verify(password, hashed_password)

e278d557a078e18a37c50b9e16389ad53259e4dd
balddrAdmin <boliklevik@gmail.com>
2026-06-13 01:26:12 +0200

    SUserReg add

--- users/schemas.py
+++ users/schemas.py
@@ -4,3 +4,10 @@ from pydantic import BaseModel, Field
 class SUserAuth(BaseModel):
     username: str = Field(min_length=1, max_length=24)
     password: str = Field(min_length=1, max_length=32)
+
+
+class SUserReg(BaseModel):
+    first_name: str = Field(min_length=1, max_length=50)
+    last_name: str | None = Field(default=None, min_length=1, max_length=50)
+    username: str = Field(min_length=1, max_length=24)
+    password: str = Field(min_length=1, max_length=32)

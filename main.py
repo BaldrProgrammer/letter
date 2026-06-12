5a8aabf201e275f16dd5e225cce3f218351f87e2
balddrAdmin <boliklevik@gmail.com>
2026-06-13 01:30:42 +0200

    include users_router & auth_router

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from users.routers import router as users_router
+from users.auth_routers import router as auth_router
+
 app = FastAPI()
+app.include_router(users_router)
+app.include_router(auth_router)
 
 app.add_middleware(
     CORSMiddleware,

9db724ce2983953f6c980b5b9dcf4cd1688c6062
balddrAdmin <boliklevik@gmail.com>
2026-06-13 01:28:08 +0200

    SUserGet add

--- users/schemas.py
+++ users/schemas.py
@@ -11,3 +11,12 @@ class SUserReg(BaseModel):
     last_name: str | None = Field(default=None, min_length=1, max_length=50)
     username: str = Field(min_length=1, max_length=24)
     password: str = Field(min_length=1, max_length=32)
+
+
+class SUserGet(BaseModel):
+    id: int
+    first_name: str
+    last_name: str
+    username: str
+    password: str
+    profile_photo: str

cd791e34d55f1a95afb7a8d3ee771328f329c677
balddrAdmin <boliklevik@gmail.com>
2026-06-14 01:47:36 +0200

    +POST /auth/reg & __pycache__ removed

--- users/auth_routers.py
+++ users/auth_routers.py
@@ -1,3 +1,20 @@
 from fastapi import APIRouter
 
-router = APIRouter(prefix='/auth')
+from sqlalchemy import insert
+from database import session_maker
+
+from users.models import User
+from users.schemas import SUserReg, SUserGet
+
+router = APIRouter(prefix='/auth', tags=['/auth'])
+
+
+@router.post('/reg')
+async def user_reg(user_data: SUserReg):
+    stmt = insert(User).values(first_name=user_data.first_name,
+                               last_name=user_data.last_name,
+                               username=user_data.username,
+                               password=user_data.password)
+    async with session_maker() as session:
+        await session.execute(stmt)
+        await session.commit()

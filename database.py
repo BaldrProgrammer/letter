806bab498bf208abc6578214f05162af56ca5a0d
balddrAdmin <boliklevik@gmail.com>
2026-06-14 01:46:36 +0200

    Base created & updated datetime types fix

--- database.py
+++ database.py
@@ -4,12 +4,12 @@ from sqlalchemy import DateTime
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncAttrs
 
-from datetime import datetime, UTC
+from datetime import datetime, timezone
 
 engine = create_async_engine(get_db_url())
 session_maker = async_sessionmaker(engine, expire_on_commit=True)
 
 
 class Base(AsyncAttrs, DeclarativeBase):
-    created_at: Mapped[datetime] = mapped_column(DateTime(), default=lambda: datetime.now(UTC))
-    updated_at: Mapped[datetime] = mapped_column(DateTime(), default=lambda: datetime.now(UTC), onupdate=lambda: datetime.now(UTC))
+    created_at: Mapped[datetime] = mapped_column(DateTime(), default=lambda: datetime.now(timezone.utc).replace(tzinfo=None))
+    updated_at: Mapped[datetime] = mapped_column(DateTime(), default=lambda: datetime.now(timezone.utc).replace(tzinfo=None), onupdate=lambda: datetime.now(timezone.utc).replace(tzinfo=None))

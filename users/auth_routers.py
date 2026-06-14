b2569fb36f16554281e54b75dbcb210ccc702212
balddrAdmin <boliklevik@gmail.com>
2026-06-14 02:01:49 +0200

    POST /auth/reg improved full

--- users/auth_routers.py
+++ users/auth_routers.py
@@ -1,20 +1,29 @@
 from fastapi import APIRouter
 
 from sqlalchemy import insert
-from database import session_maker
+from sqlalchemy.exc import SQLAlchemyError
 
+from database import session_maker
 from users.models import User
 from users.schemas import SUserReg, SUserGet
+from users.auth import get_hashed_password
 
 router = APIRouter(prefix='/auth', tags=['/auth'])
 
 
 @router.post('/reg')
-async def user_reg(user_data: SUserReg):
+async def user_reg(user_data: SUserReg) -> dict:
+    hashed_password = get_hashed_password(user_data.password)
     stmt = insert(User).values(first_name=user_data.first_name,
                                last_name=user_data.last_name,
                                username=user_data.username,
-                               password=user_data.password)
+                               password=hashed_password)
     async with session_maker() as session:
         await session.execute(stmt)
-        await session.commit()
+        try:
+            await session.commit()
+        except SQLAlchemyError as e:
+            await session.rollback()
+            raise e
+
+    return {'ok': True}

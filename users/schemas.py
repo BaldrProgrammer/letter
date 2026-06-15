f0d8604d5febc0523ac254ba7821f03352f50cce
balddrAdmin <boliklevik@gmail.com>
2026-06-15 22:01:40 +0200

    user schemas email added

--- users/schemas.py
+++ users/schemas.py
@@ -2,21 +2,21 @@ from pydantic import BaseModel, Field
 
 
 class SUserAuth(BaseModel):
-    username: str = Field(min_length=1, max_length=24)
-    password: str = Field(min_length=1, max_length=32)
+    email: str = Field(pattern='/^[a-zA-Z0-9._%+-]+@gmail\\.com$/')
 
 
 class SUserReg(BaseModel):
     first_name: str = Field(min_length=1, max_length=50)
     last_name: str | None = Field(default=None, min_length=1, max_length=50)
+    email: str = Field(pattern='/^[a-zA-Z0-9._%+-]+@gmail\\.com$/')
     username: str = Field(min_length=1, max_length=24)
-    password: str = Field(min_length=1, max_length=32)
 
 
 class SUserGet(BaseModel):
     id: int
     first_name: str
     last_name: str | None
+    email: str
     username: str
-    password: str
+    password: str | None
     profile_photo: str | None

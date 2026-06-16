65b1932aa6ef3229e6c3481f4d98a8c722df7a9d
balddrAdmin <boliklevik@gmail.com>
2026-06-16 17:34:51 +0200

    +SEmail & SUserAuth-SUserReg change & SUserGet Config from_attributes=True

--- users/schemas.py
+++ users/schemas.py
@@ -1,17 +1,22 @@
 from pydantic import BaseModel, Field
 
 
+class SEmail(BaseModel):
+    email: str = Field(pattern='^[a-zA-Z0-9._%+-]+@gmail\\.com$')
+
+
 class SUserAuth(BaseModel):
-    email: str = Field(pattern='/^[a-zA-Z0-9._%+-]+@gmail\\.com$/')
+    email: str = Field(pattern='^[a-zA-Z0-9._%+-]+@gmail\\.com$')
+    code: int = Field(ge=100_000, le=999_999)
+    is_login: bool
 
 
 class SUserReg(BaseModel):
     first_name: str = Field(min_length=1, max_length=50)
     last_name: str | None = Field(default=None, min_length=1, max_length=50)
-    email: str = Field(pattern='/^[a-zA-Z0-9._%+-]+@gmail\\.com$/')
+    email: str = Field(pattern='^[a-zA-Z0-9._%+-]+@gmail\\.com$')
     username: str = Field(min_length=1, max_length=24)
     password: str | None = Field(min_length=1, max_length=32)
-    code: int = Field(ge=100_000, le=999_999)
 
 
 class SUserGet(BaseModel):
@@ -22,3 +27,6 @@ class SUserGet(BaseModel):
     username: str
     password: str | None
     profile_photo: str | None
+
+    class Config:
+        from_attributes = True

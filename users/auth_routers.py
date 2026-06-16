d255a971729afa9ed802b816e7548fe03b692137
balddrAdmin <boliklevik@gmail.com>
2026-06-16 17:35:13 +0200

    authorization works successful

--- users/auth_routers.py
+++ users/auth_routers.py
@@ -7,40 +7,40 @@ from settings import settings
 from database import session_maker
 from cache.redis_object import RedisCacheBackend
 from users.models import User
-from users.schemas import SUserAuth, SUserReg, SUserGet
-from users.auth import verify_password, jwt_encode, send_code
+from users.schemas import SEmail, SUserAuth, SUserReg, SUserGet
+from users.auth import jwt_encode, send_code
 
 router = APIRouter(prefix='/auth', tags=['/auth'])
-redis = RedisCacheBackend(settings.REDIS_URL, 60)
+redis = RedisCacheBackend(settings.REDIS_URL, 180)
 
 
 @router.post('/send_code')
-async def send_code_endpoint(send_to: str):
-    code = send_code(send_to)
-    await redis.set(f'{send_to}:code', code)
+async def send_code_endpoint(send_to: SEmail):
+    code = send_code(send_to.email)
+    await redis.set(f'{send_to.email}:code', code)
     return {'ok': True}
 
 
 @router.post('/check_code')
-async def user_log(response: Response, email: str, code: int, is_login: bool):
-    stmt = select(User).where(User.email == email)
+async def user_log(response: Response, auth_data: SUserAuth):
+    stmt = select(User).where(User.email == auth_data.email)
     async with session_maker() as session:
-        code_from_redis = await redis.get(f'{email}:code')
+        code_from_redis = await redis.get(f'{auth_data.email}:code')
         if code_from_redis:
-            if code == int(code_from_redis):
-                if not is_login:
+            if auth_data.code == int(code_from_redis):
+                if not auth_data.is_login:
                     return {'ok': True}
 
                 result = await session.execute(stmt)
                 result = result.scalars().one_or_none()
                 if result:
-                    result_user = SUserGet(**result.to_dict())
+                    result_user = SUserGet.model_validate(result)
                     response.set_cookie('access_token', jwt_encode({'user_id': result_user.id}))
                     return {'ok': True, 'access_token': jwt_encode({'user_id': result_user.id})}
 
-                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='email not found')
-            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='code does not pass')
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='code expired or not sent')
+                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='die Email nicht gefunden')
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='der Code passiert nicht')
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='der Code ist abgelaufen oder nicht gesendet')
 
 
 @router.post('/reg')

d894bbd75c7209b9c38cefbc731737c95dc2d3fb
balddrAdmin <boliklevik@gmail.com>
2026-06-18 16:57:52 +0200

    GET /chats/all

--- chats/routers.py
+++ chats/routers.py
@@ -1,3 +1,18 @@
 from fastapi import APIRouter
 
+from sqlalchemy import select
+from typing import List
+
+from database import session_maker
+from chats.models import Chat
+from chats.schemas import SChatGet
+
 router = APIRouter(prefix='/chats', tags=['/chats'])
+
+
+@router.get('/all')
+async def get_all() -> List[SChatGet]:
+    stmt = select(Chat)
+    async with session_maker() as session:
+        result = await session.execute(stmt)
+        return result.scalars().all()

85e42a9267cb08b37ff675f2b506bcb4890fe223
balddrAdmin <boliklevik@gmail.com>
2026-06-21 01:18:25 +0200

    WebSocket init

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from users.routers import router as users_router
 from users.auth_routers import router as auth_router
 from chats.routers import router as chats_router
 from messages.routers import router as messages_router
+from websocket.router import router as websocket_router
 from models import User, Chat, Message
 
 app = FastAPI()
@@ -12,6 +13,7 @@ app.include_router(users_router)
 app.include_router(auth_router)
 app.include_router(chats_router)
 app.include_router(messages_router)
+app.include_router(websocket_router)
 
 app.add_middleware(
     CORSMiddleware,
--- websocket/router.py
+++ websocket/router.py
@@ -1,5 +1,25 @@
 from fastapi import APIRouter, WebSocket
 
+from jose import JWTError
+from users.auth import jwt_decode
+
 router = APIRouter(prefix='/ws')
 
 connections = {}
+
+
+@router.websocket('/')
+async def websocket(ws: WebSocket):
+    access_token = ws.headers.get('access_token')
+    if not access_token:
+        raise ws.close(code=1008, reason='auth cookies fehlen')
+
+    try:
+        user_id = jwt_decode(access_token)['user_id']
+    except JWTError:
+        raise ws.close(code=1008, reason='der Token ist fehlerhaft')
+
+    await ws.accept()
+    connections[user_id] = ws
+    while True:
+        ... # тут код позже будет

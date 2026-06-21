b94c6e0de8af9cf82e6e4e30b5ac9008ec068b61
balddrAdmin <boliklevik@gmail.com>
2026-06-21 23:46:43 +0200

    websocket case create_chat

--- websocket/utils.py
+++ websocket/utils.py
@@ -1,6 +1,6 @@
 from fastapi import WebSocket
 
-from sqlalchemy import insert
+from sqlalchemy import select
 from sqlalchemy.exc import SQLAlchemyError
 from database import session_maker
 
@@ -12,18 +12,20 @@ from messages.schemas import SMessageAdd
 
 async def create_chat(add_data: SChatAdd):
     async with session_maker() as session:
-        new_chat = Chat(title=add_data.title)
-        for user_id in add_data.users:
-            user = await session.get(User, user_id)
-            if user:
-                new_chat.users.append(user)
-
+        users = (
+            await session.execute(
+                select(User).where(User.id.in_(add_data.users))
+            )
+        ).scalars().all()
+        new_chat = Chat(title=add_data.title, users=users)
         session.add(new_chat)
+
         try:
             await session.commit()
         except SQLAlchemyError as e:
             await session.rollback()
             raise e
+
 
 async def send_message(ws: WebSocket, message_data: SMessageAdd):
     await ws.send_json(message_data.model_dump())

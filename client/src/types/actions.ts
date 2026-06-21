cc799ee0c959b805e7e8115d4fe154eb519af021
Karinka <karina.rogalevich@mail.ru>
2026-06-21 14:01:14 +0200

    habe die Schnittstelle fuer User gemacht

--- client/src/types/actions.ts
+++ client/src/types/actions.ts
@@ -3,10 +3,19 @@ export interface check_code{
     code:number,
     is_Login:boolean,
 }
+
 export interface Reg{
     first_name: string,
     last_name:string,
     email:string,
     username:string,
     password?:string,
+}
+
+export interface User{
+    first_name: string,
+    last_name:string,
+    email:string,
+    username:string,
+    fileUrl?: string
 }

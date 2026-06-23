5ed94f0e41c959feb1b51a7fd851a4888d35a8cf
balddrAdmin <boliklevik@gmail.com>
2026-06-23 16:48:51 +0200

    GET /chats/users

--- chats/routers.py
+++ chats/routers.py
@@ -6,7 +6,7 @@ from typing import List
 
 from database import session_maker
 from chats.models import Chat
-from chats.schemas import SChatAdd, SChatGet
+from chats.schemas import SChatGet
 from users.schemas import SUserGet
 
 router = APIRouter(prefix='/chats', tags=['/chats'])
@@ -18,3 +18,15 @@ async def get_all() -> List[SChatGet]:
     async with session_maker() as session:
         result = await session.execute(stmt)
         return result.scalars().all()
+
+
+@router.get('/users')
+async def get_chat_users(chat_id: int) -> List[SUserGet]:
+    async with session_maker() as session:
+        stmt = select(Chat).where(Chat.id == chat_id).options(selectinload(Chat.users))
+        chat = await session.execute(stmt)
+        chat = chat.scalars().one_or_none()
+        if not chat:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='der Chat ist nicht gefunden')
+
+        return chat.users

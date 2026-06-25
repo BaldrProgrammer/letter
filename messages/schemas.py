aae94f0ef7948232f65f5d43eed86265b8d1e3ab
balddrAdmin <boliklevik@gmail.com>
2026-06-25 20:36:03 +0200

    SMessageGet

--- messages/schemas.py
+++ messages/schemas.py
@@ -5,3 +5,12 @@ class SMessageAdd(BaseModel):
     text: str = Field(min_length=1, max_length=8192)
     sender_id: int = Field(ge=1)
     chat_id: int = Field(ge=1)
+
+
+class SMessageGet(BaseModel):
+    id: int = Field(ge=1)
+    text: str = Field(min_length=1, max_length=8192)
+    is_edited: bool = Field(...)
+    is_forwarded: bool = Field(...)
+    sender_id: int = Field(ge=1)
+    chat_id: int = Field(ge=1)

113332b8c7449a2217d33ad9d6727ca290f8f166
balddrAdmin <boliklevik@gmail.com>
2026-06-28 20:35:36 +0200

    set_profile_photo will be on HTTP

--- websocket/funcs/user_funcs.py
+++ websocket/funcs/user_funcs.py
@@ -4,23 +4,3 @@ from sqlalchemy.exc import SQLAlchemyError
 from database import session_maker
 
 from users.models import User
-
-
-async def set_profile_photo(user_id: int, profile_photo: bytes):
-    if not (str(user_id) in os.listdir('storage')):
-        os.mkdir(f'storage/{user_id}')
-
-    newpath = f'storage/{user_id}/profile_photo.jpg'
-    with open(newpath, 'wb') as file:
-        file.write(profile_photo)
-
-    stmt = update(User).where(User.id == user_id).values(profile_photo=newpath)
-    async with session_maker() as session:
-        await session.execute(stmt)
-        try:
-            await session.commit()
-        except SQLAlchemyError as e:
-            await session.rollback()
-            raise e
-
-    return {'ok': True, 'newpath': newpath}

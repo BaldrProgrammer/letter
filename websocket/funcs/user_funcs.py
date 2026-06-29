5966188ec19436ddd51ab47e610c80ee925bf90b
balddrAdmin <boliklevik@gmail.com>
2026-06-29 14:14:31 +0200

    enter_online()

--- websocket/funcs/user_funcs.py
+++ websocket/funcs/user_funcs.py
@@ -1,11 +1,18 @@
-from fastapi import WebSocket
-
-import os
 from sqlalchemy import update
 from sqlalchemy.exc import SQLAlchemyError
 from database import session_maker
 
+from datetime import datetime, timezone
+
 from users.models import User
 
 
-
+async def enter_online(user_id):
+    stmt = update(User).where(User.id == user_id).values(online = True, last_online = datetime.now(timezone.utc))
+    async with session_maker() as session:
+        await session.execute(stmt)
+        try:
+            await session.commit()
+        except SQLAlchemyError as e:
+            await session.rollback()
+            raise e
